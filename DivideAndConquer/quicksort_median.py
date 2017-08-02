897d7eb36a0f01c2ce1667ffeb3750cd01cf8fa2
Luke Biggerstaff <luke.biggerstaff@gmail.com>
2017-08-02 08:30:24 -0500

    updated unit tests to cover the median value selection in quicksort

--- DivideAndConquer/quicksort_median.py
+++ DivideAndConquer/quicksort_median.py
@@ -12,7 +12,7 @@ def quicksort(lst, left, right):
 
 def partition(lst, left, right):
 
-    pivot = lst[left]
+    pivot = choose_pivot(lst, left, right)
     i = left + 1
     for j in range(i, right+1):
         if lst[j] < pivot:
@@ -20,6 +20,35 @@ def partition(lst, left, right):
             i += 1
     lst[left], lst[i-1] = lst[i-1], lst[left]
     return i - 1
+
+def choose_pivot(lst, left, right):
+    middle = find_middle(lst, left, right)
+    if lst[left] < lst[middle]:
+        if lst[middle] < lst[right]:
+            lst[left], lst[middle] = lst[middle], lst[left]
+            return lst[left]
+        elif lst[left] < lst[right]:
+            lst[left], lst[right] = lst[right], lst[left]
+            return lst[left]
+        else:
+            return lst[left]
+    else:
+        if lst[left] < lst[right]:
+            return lst[left]
+        elif lst[right] < lst[middle]:
+            lst[left], lst[middle] = lst[middle], lst[left]
+            return lst[left]
+        else:
+            lst[left], lst[right] = lst[right], lst[left]
+            return lst[left]
+
+def find_middle(lst, left, right):
+    sublist_len = right - left + 1
+    if sublist_len % 2 is 0:
+        middle = sublist_len // 2 - 1
+    else:
+        middle = sublist_len // 2
+    return middle + left
 
 
 if __name__ == '__main__':
