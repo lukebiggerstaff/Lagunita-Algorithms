26ab554a1a9c6b9a880f10082289b38cc48732de
luke biggerstaff <luke.biggerstaff@gmail.com>
2017-11-07 13:49:36 -0600

    knapsack_big.py reduces space usage to O(n) and will work for assignment 3.2

--- dynamic/knapsack_big.py
+++ dynamic/knapsack_big.py
@@ -11,22 +11,22 @@ def read_file(file):
 def return_max_value_knapsack(items, weight):
     n = len(items)
     w = weight + 1
-    a = [[] for i in range(w)]
-    for lst in a: lst.append(0)
+    a = [0 for i in range(w)]
     for i in range(1, n):
+        temp = []
         for j in range(w):
             ci_w = items[i][1]
             ci_v = items[i][0]
-            pv_at_w = a[j][i-1]
-            cv_w_item = a[j-ci_w][i-1] + ci_v if ci_w <= j else 0
+            pv_at_w = a[j]
+            cv_w_item = a[j-ci_w] + ci_v if ci_w <= j else 0
             max_value = max(pv_at_w,cv_w_item)
-            a[j].append(max_value)
-    #return a[w-1][n-1]
-    return a
+            temp.append(max_value)
+        a = list(temp)
+    return a[w-1]
 
 
 if __name__ == '__main__':
     items = read_file(sys.argv[1])
     result = return_max_value_knapsack(items, items[0][0])
-    for line in result:
-        print(line)
+    print(result)
+
